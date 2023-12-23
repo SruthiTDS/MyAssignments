7dbde706983e7e222dac887c15527e4a4438dcfb
SruthiTDS <153560329+SruthiTDS@users.noreply.github.com>
2023-12-22 22:17:35 -0500

    Add files via upload
    week2_buttonInteractions

---
+++ week2/day2/RadioButton.java
@@ -0,0 +1,32 @@
+package week2.day2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.*;
+public class RadioButton {
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.leafground.com/radio.xhtml");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.xpath("//label[text()='Firefox']/preceding-sibling::div")).click();
+		driver.findElement(By.xpath("//label[text()='Chennai']/preceding-sibling::div")).click();
+		boolean button=driver.findElement(By.xpath("//label[text()='Chennai']/preceding-sibling::div")).isSelected();
+		System.out.println(button);
+		driver.findElement(By.xpath("//label[text()='Chennai']/preceding-sibling::div")).click();
+		boolean button1=driver.findElement(By.xpath("//label[text()='Chennai']/preceding-sibling::div")).isSelected();
+		System.out.println(button1);
+		//System.out.println(driver.findElement(By.xpath("//label[text()='Chennai']/preceding-sibling::div")).isDisplayed());
+		//System.out.println(driver.findElement(By.xpath("//label[text()='Chennai']/preceding-sibling::div")).isEnabled());
+		String b=driver.findElement(By.xpath("(//div[contains(@class,'ui-state-active')])[5]/following::label")).getText();
+		System.out.println(b);
+		boolean age=driver.findElement(By.xpath("(//div[contains(@class,'ui-state-active')])[5]")).isSelected();
+		//Always displaying false for isSelected need to check
+		System.out.println(age);
+		
+		
+	}
+
+}
