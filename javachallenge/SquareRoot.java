62e1856770ff3124ec7072876f0f919f5400b40a
SruthiTDS <153560329+SruthiTDS@users.noreply.github.com>
2023-12-29 14:30:02 -0500

    Add files via upload
    java challenge

---
+++ javachallenge/SquareRoot.java
@@ -0,0 +1,14 @@
+package javachallenge;
+
+public class SquareRoot {
+	public static void main(String[] args) {
+		int x=8; 
+		System.out.println(Math.sqrt(x));
+		//Need to write without any built in functions
+
+
+
+	}
+}
+
+
